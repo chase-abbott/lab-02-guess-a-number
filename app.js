3297bd23e4bcc8554cd050782ec57ad0bf067a75
chase-abbott <cabbott93@gmail.com>
2021-03-24 15:43:27 -0700

    add some styles

--- app.js
+++ app.js
@@ -4,10 +4,12 @@ const userInput = document.getElementById('user-input');
 const guessButton = document.getElementById('guess');
 const retryButton = document.getElementById('retry');
 const results = document.getElementById('results');
+const main = document.getElementById('main');
+const wrongText = document.getElementById('wrong-text');
 
 
 // initialize state
-let clicks = 3;
+let clicks = 5;
 let correctAnswer = Math.round(Math.random() * 20);
 
 // set event listeners to update state and DOM
@@ -23,28 +25,35 @@ guessButton.addEventListener('click', () => {
     };
 
     if (checkIfCorrect(userInput.value, correctAnswer) === 'equal') {
-        guessButton.disabled = true;
         guessButton.style.display = 'none';
+        retryButton.style.display = 'block';
         results.textContent = 'Great Job! You win!';
     };
 
     if (clicks < 1) {
-        guessButton.disabled = true;
         guessButton.style.display = 'none';
+        retryButton.style.display = 'block';
         results.textContent = "You lose!";
+        main.classList.add('wrong');
+        wrongText.classList.remove('hidden');
     };  
 
     if (checkIfCorrect(userInput.value, correctAnswer) === 'equal' && clicks <= 1) {
-        guessButton.disabled = true;
         guessButton.style.display = 'none';
+        retryButton.style.display = 'block';
+        main.classList.add('wrong');
+        wrongText.classList.remove('hidden');
         results.textContent = 'Great Job! You win!';
     };   
 });
 
 retryButton.addEventListener('click', () => {
-    guessButton.disabled = false;
+    // location.reload(); can be used instead but reloads the page and feels really slow
     guessButton.style.display = 'block';
+    retryButton.style.display = 'none';
     results.textContent = '';
+    main.classList.remove('wrong');
+    wrongText.classList.add('hidden');
     clicks = 3;
     correctAnswer = Math.round(Math.random() * 20);
 });
