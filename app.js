265afbf975963508588b5903c582dc2cbe014a18
chase-abbott <cabbott93@gmail.com>
2021-03-24 13:24:43 -0700

    set state and add buttons

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 // import functions and grab DOM elements
 import { checkIfCorrect } from './test/utils.js';
 const userInput = document.getElementById('user-input');
-const guess = document.getElementById('guess');
-const retry = document.getElementById('retry');
+const guessButton = document.getElementById('guess');
+const retryButton = document.getElementById('retry');
 const results = document.getElementById('results');
 
-console.log(userInput, guess, retry, results);
+
 // initialize state
+let clicks = 3;
+let correctAnswer = Math.round(Math.random() * 20);
 
 // set event listeners to update state and DOM
+guessButton.addEventListener('click', () => {
+    
+})
+
+retryButton.addEventListener('click', () => {
+    console.log('button');
+})
