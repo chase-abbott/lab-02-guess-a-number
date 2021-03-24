c37c0949cdd907b6fea1743644bf1c2d2429730c
chase-abbott <cabbott93@gmail.com>
2021-03-24 11:19:03 -0700

    test stuff

--- test/example.test.js
+++ test/example.test.js
@@ -4,15 +4,9 @@
 const test = QUnit.test;
 
 test('time to test a function', (expect) => {
-    //Arrange
-    // Set up your arguments and expectations
-    const expected = true;
-    
-    //Act 
-    // Call the function you're testing and set the result to a const
-    const actual = true;
-
     //Expect
     // Make assertions about what is expected versus the actual result
+     // three arguments: acutal, expected, and some description
     expect.equal(actual, expected);
+   
 });
