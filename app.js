27005010c2f9bc950b1236f57ca1502b6e7d53ab
chase-abbott <cabbott93@gmail.com>
2021-03-24 14:25:28 -0700

    add disable feature when guesses hit 0 or correct answer is found

--- app.js
+++ app.js
@@ -9,21 +9,30 @@ const results = document.getElementById('results');
 // initialize state
 let clicks = 3;
 let correctAnswer = Math.round(Math.random() * 20);
+console.log(correctAnswer);
 
 // set event listeners to update state and DOM
 guessButton.addEventListener('click', () => {
     clicks--;
+   console.log(clicks);
     if (checkIfCorrect(userInput.value, correctAnswer) === 'less than') {
-        results.textContent = 'Go Lower! The answer is less than that!';
+        results.textContent = `The answer is less than that! You have ${clicks} tries left!`;
     };
     if (checkIfCorrect(userInput.value, correctAnswer) === 'greater than') {
-        results.textContent = 'Go Higher! The answer is greater than that!';
-    }
+        results.textContent = `The answer is greater than that! You have ${clicks} tries left!`;
+    };
     if (checkIfCorrect(userInput.value, correctAnswer) === 'equal') {
-        results.textContent = 'Great Job! You got it!';
-    }
+        results.textContent = 'Great Job! You win!';
+    };
+    if (clicks <= 0) {
+        guessButton.disabled = true;
+        results.textContent = "You lose!";
+       
+    }    
 })
 
 retryButton.addEventListener('click', () => {
+    guessButton.disabled = false;
+    results.textContent = '';
     
 })
